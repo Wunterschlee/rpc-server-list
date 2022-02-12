ce4430f8cce6ee0f4aee344f05f8c4aa1b087bef
Wunterschlee <90971259+Wunterschlee@users.noreply.github.com>
2022-02-12 23:51:42 +0500

    Update server-list-rpc.py

--- server-list-rpc.py
+++ server-list-rpc.py
@@ -1,11 +1,13 @@
 from xmlrpc.server import SimpleXMLRPCServer
 
-local_ip = "http://server-list-rpc.herokuapp.com"
+local_ip = 'localhost'
 
 def get_servers ():
+    print ("entered sus")
     return "sus"
 
-rpc_server.register_function(get_servers, "sus")
+
 rpc_server = SimpleXMLRPCServer((local_ip, 6789),allow_none=True)
+rpc_server.register_function(get_servers, "sus")
 rpc_server.serve_forever()
     
